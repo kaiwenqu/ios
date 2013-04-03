8166b9ebdc479f624e3985ad60c9c1ad043fe2eb
Kevin Qu <kevinqu@Kevins-MacBook-Pro.local>
2013-04-02 17:15:29 -0700

    second test

--- consoleApp/main.m
+++ consoleApp/main.m
@@ -7,14 +7,41 @@
 //
 
 #import <Foundation/Foundation.h>
+@interface Random : NSObject
+@property int test;
+-(int) add: (int) a;
+-(int) sub: (int) a;
+@end
+
+@implementation Random
+
+@synthesize test;
+
+-(int) add:(int) a
+{
+    a++;
+    return a;
+}
+
+-(int) sub:(int) a
+{
+    a--;
+    return a;
+}
+
+@end
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
+        Random *rand;
+        rand = [[Random alloc] init];
+        NSLog(@"%i\n", rand.test);
+        rand.test = 10;
+        NSLog(@"%i\n", rand.test);
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello, World!%i %i", [rand add: 2], [rand sub: 6]);
         
     }
     return 0;
